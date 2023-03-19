7ec0b51c06432c25a6ff2d279955c103f577a08f
sofiaboucap <sofiabouca@tecnico.ulisboa.pt>
2023-03-19 13:11:23 +0000

    classes e funcoes feitas

--- python/sofia bouca/main.py
+++ python/sofia bouca/main.py
@@ -6,6 +6,7 @@ suits = {'Clubs', 'Diamonds', 'Hearts', 'Spades'}
 ranks = {'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'King', 'Queen', 'Jack', 'Ace'}
 values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'King': 10, 'Queen': 10, 'Jack': 10, 'Ace': 11} 
 
+player_doing_stuff = True
 
 #criar uma carta
 class card:
@@ -60,7 +61,7 @@ class hand:
             self.value -= 10
             self.aces -= 1
 
-class bet: #para atualizar apostas
+class chips: #para atualizar apostas
     def __init__(self):
         self.total = 0
         self.bet = 0
@@ -70,6 +71,73 @@ class bet: #para atualizar apostas
     
     def win_bet(self):
         self.total += self.bet
+
+
+#pergunta ao utilizador qual vai ser o valor da aposta 
+def ask_bet(bet):
+    while True:
+        chips.bet = input('How much do you want to bet? ')
+        if chips.bet != int:
+            print('Error: please write a number: ')
+        else: 
+            if chips.bet < 2 or chips.bet > 500:
+                print('Error: your bet must be between 2eur-500eur')
+            else:
+                break
+
+
+def hit(deck, hand):
+    hand.add_card(deck.deal()) #distribuimos uma carta do baralho para a adicionar à mao
+    hand.change_value_ace() #para checkar se temos de atualizar o valor do ás
+
+
+#pergunta ao jogador o que quer fazer 
+def hit_vs_stand(deck, hand):
+    global player_doing_stuff #se nao declarar como variavel global dentro desta funcao, seria criada uma nova variavel 'player_doing_stuff'.
+                              #ou seja, alterar o valor desta variavel dentro desta funcao nao alteraria em nada o valor fora da funcao
+
+    while True:
+        question = input('\n Please choose if you want to hit ("h") or stand ("s"): ')
+
+        if question == 'h':
+            hit(deck, hand)
+        elif question == 's':
+            print("Player stands, Dearler's turn.")
+            player_doing_stuff = False #utilizador deixa de jogar. é a vez do dealer
+        else: 
+            print('Error: invalid command. Please try again')
+            continue
+        break
+
+def hide_dealer(player, dealer):
+    print("\n --> Dealer's Hand: ")
+    print("<face down>")
+    print(dealer.cards[1]) #printa o segundo elemento do vetor 'cards' da classe 'hand' do dealer
+    print("\n --> Player's Hand: ")
+    for card in player.cards: #printa cada carta do utilizador numa linha diferente
+        print(card)
+    
+def show_cards(player, dealer):
+    print("\n --> Dealer's Hand: ")
+    for card in dealer.cards: 
+        print(card)
+    print("\n --> Dealer's Value: ", dealer.value)
+    print("\n --> Player's Hand: ")
+    for card in player.cards: 
+        print(card)
+    print("\n --> Player's Value: ", player.value)
+
+
+
+    
+
+        
+
+
+
+
+
+        
 
 
 
